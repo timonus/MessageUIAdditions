7994b0ea0e7443a78fd5a3765c64acd4b32511e9
Tim Johnsen <timonus@gmail.com>
2018-02-11 17:03:26 -0800

    App specific email.

--- MessageUI + TJAdditions.m
+++ MessageUI + TJAdditions.m
@@ -6,7 +6,7 @@
 #import <sys/utsname.h>
 #import <sys/sysctl.h>
 
-#define FEEDBACK_RECIPIENT [NSString stringWithFormat:@"%@@%@.com", @"tijoinc", @"gmail"]		// make this the email you'd like to receive feedback at
+#define FEEDBACK_RECIPIENT [NSString stringWithFormat:@"%@@%@.com", @"tijoinc+closeup", @"gmail"]		// make this the email you'd like to receive feedback at
 
 #pragma mark - MFMailComposeViewController
 
