c871591a929a8fec764dd0a21435f7d0cb4598f6
Tim Johnsen <timonus@gmail.com>
2020-07-05 21:08:44 -0700

    Fix up case where mail composer isn't available.

--- MessageUI + TJAdditions.m
+++ MessageUI + TJAdditions.m
@@ -12,27 +12,52 @@
 
 #pragma mark Class Methods
 
-+ (void)presentInViewController:(UIViewController *)viewController withSubject:(NSString *)subject messageBody:(NSString *)messageBody isHTML:(BOOL)isHTML
++ (void)presentInViewController:(UIViewController *)viewController
+                    withSubject:(NSString *)subject
+                    messageBody:(NSString *)messageBody
+                         isHTML:(BOOL)isHTML
 {
-    MFMailComposeViewController *controller = [MFMailComposeViewController new];
-    
-    [controller setSubject:subject];
-    [controller setMessageBody:messageBody isHTML:isHTML];
-    [controller setMailComposeDelegate:controller];
-    
-    [viewController presentViewController:controller animated:YES completion:nil];
+    [self presentInViewController:viewController
+                 withToRecipients:nil
+                          subject:subject
+                      messageBody:messageBody
+                           isHTML:isHTML];
 }
 
-+ (void)presentInViewController:(UIViewController *)viewController withToRecipients:(NSArray *)recipients subject:(NSString *)subject messageBody:(NSString *)messageBody isHTML:(BOOL)isHTML
++ (void)presentInViewController:(UIViewController *)viewController
+               withToRecipients:(NSArray *)recipients
+                        subject:(NSString *)subject
+                    messageBody:(NSString *)messageBody
+                         isHTML:(BOOL)isHTML
 {
-    MFMailComposeViewController *controller = [MFMailComposeViewController new];
-    
-    [controller setSubject:subject];
-    [controller setMessageBody:messageBody isHTML:isHTML];
-    [controller setMailComposeDelegate:controller];
-    [controller setToRecipients:recipients];
-    
-    [viewController presentViewController:controller animated:YES completion:nil];
+    if ([MFMailComposeViewController canSendMail]) {
+        MFMailComposeViewController *controller = [MFMailComposeViewController new];
+
+        [controller setSubject:subject];
+        [controller setMessageBody:messageBody isHTML:isHTML];
+        [controller setMailComposeDelegate:controller];
+        [controller setToRecipients:recipients];
+
+        [viewController presentViewController:controller animated:YES completion:nil];
+    } else {
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Set up Mail"
+                                                                                 message:@"You haven't set up the Mail app on your device."
+                                                                          preferredStyle:UIAlertControllerStyleAlert];
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Go to Mail" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            NSString *urlString = @"mailto:";
+            if (recipients.count == 1) {
+                urlString = [urlString stringByAppendingString:recipients.firstObject];
+            }
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString] options:@{} completionHandler:nil];
+        }]];
+        if (recipients.count == 1) {
+            [alertController addAction:[UIAlertAction actionWithTitle:@"Copy email address" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [[UIPasteboard generalPasteboard] setString:recipients.firstObject];
+            }]];
+        }
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Ignore" style:UIAlertActionStyleCancel handler:nil]];
+        [viewController presentViewController:alertController animated:YES completion:nil];
+    }
 }
 
 + (void)presentFeedbackEmailViewControllerInViewController:(UIViewController *)viewController
